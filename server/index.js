cca2b2ec2c9b4312add2a158ce8529a75cc92938
Wilowayne De La Cruz G <wilo0087@gmail.com>
2019-02-28 14:10:31 -0400

    Working on server side

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
-// import fs from 'fs';
+import fs from 'fs';
+import sha256 from 'sha256';
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
@@ -9,7 +10,24 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname, 'node_modules')));
 
 app.get('**', (req, res) => {
-  res.send('hello world');
+
+  // App's index.html file
+  const indexFile = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+
+  // App's app.js file
+  // const mainJsFile = fs.readFileSync(path.join(__dirname, 'public', 'js', 'app.js'), 'utf8');
+  const mainJsFile = 'esta es el js1';
+
+  // App's app.css file
+  // const mainCssFile = fs.readFileSync(path.join(__dirname, 'public', 'css', 'main.min.css'), 'utf8');
+  const mainCssFile = ['esta es el Js1'];
+
+  // Add a variable query value to each of the resources url's, therefore avoiding cache when neces
+  const finalIndexFile = indexFile
+    .replace('/* ::APP_JS:: */', `app.js?q=${sha256(mainJsFile).slice(0, 5)}"`)
+    .replace('/* ::APP_CSS:: */', `main.min.css?q=${sha256(mainCssFile).slice(0, 5)}"`);
+
+  return res.send(finalIndexFile);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
