742e2d37073e36fe3344d98a6b53faea83f8ec31
Wilowayne De La Cruz G <wilo0087@gmail.com>
2019-03-01 16:05:31 -0400

    Making webpack funcional for js

--- server/index.js
+++ server/index.js
@@ -6,16 +6,15 @@ import sha256 from 'sha256';
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, 'node_modules')));
+app.use(express.static('public'))
+// app.use(express.static(path.resolve(__dirname, 'node_modules')));
 
-app.get('**', (req, res) => {
+app.get('/', (req, res) => {
   // App's index.html file
   const indexFile = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
 
   // App's app.js file
-  // const mainJsFile = fs.readFileSync(path.join(__dirname, 'public', 'js', 'app.js'), 'utf8');
-  const mainJsFile = 'esta es el js1';
+  const mainJsFile = fs.readFileSync(path.join(__dirname, '../public', 'js', 'app.js'), 'utf8');
 
   // App's app.css file
   // mainCssFile = fs.readFileSync(path.join(__dirname, 'public', 'css', 'main.min.css'), 'utf8');
@@ -23,8 +22,8 @@ app.get('**', (req, res) => {
 
   // Add a variable query value to each of the resources url's, therefore avoiding cache when neces
   const finalIndexFile = indexFile
-    .replace('/* ::APP_JS:: */', `app.js?q=${sha256(mainJsFile).slice(0, 5)}"`)
-    .replace('/* ::APP_CSS:: */', `main.min.css?q=${sha256(mainCssFile).slice(0, 5)}"`);
+    .replace('/* ::APP_CSS:: */', `css/main.min.css?q=${sha256(mainCssFile).slice(0, 5)}`)
+    .replace('/* ::APP_JS:: */', `js/app.js?q=${sha256(mainJsFile).slice(0, 5)}`);
 
   return res.send(finalIndexFile);
 });
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,31 @@
+/**
+ * @author Wilowayne De La Cruz <wilo0087@gmail.com>
+ * @desc Webpack module bundler for this project
+ */
+
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, './public/js'),
   },
-  
   watchOptions: {
     aggregateTimeout: 300,
     poll: 400,
